a32f6042e0e9d009d99439776c1c04f711587386
Roberto Seba <roberto.seba@melhorenvio.com>
2024-03-02 17:35:06 -0300

    wip

--- src/app/customer/customer.controller.ts
+++ src/app/customer/customer.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpStatus,
-  Param,
-  ParseIntPipe,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CustomerService } from './customer.service';
 import { TStatement } from './type/statement.type';
 import {
