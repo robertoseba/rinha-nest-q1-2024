be827485e5bbeb7c7217339a335a6d95cdda7c7f
Roberto Seba <roberto.seba@melhorenvio.com>
2024-03-04 11:03:08 -0300

    wip

--- src/infra/db/seeder.ts
+++ src/infra/db/seeder.ts
@@ -1,17 +1,21 @@
 import { Logger } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { Customer } from '../../app/customer/entity/customer.entity';
 
 export class Seeder {
   private readonly logger = new Logger(Seeder.name);
 
   async seed(dataSource: DataSource): Promise<void> {
-    this.logger.log('Seeding Database');
-    dataSource.synchronize(true);
-    const runner = dataSource.createQueryRunner();
-    const result = await runner.manager
-      .query(`INSERT INTO customers (name) VALUES
-		('o barato sai caro');
-		`);
-    this.logger.log(result.raw);
+    this.logger.log('Seeding Database...');
+
+    await dataSource.synchronize(true);
+
+    const repo = dataSource.getRepository(Customer);
+
+    await repo.save({ name: 'o barato sai caro', limit: 1000 * 100 });
+    await repo.save({ name: 'zan corp ltda', limit: 800 * 100 });
+    await repo.save({ name: 'les cruders', limit: 10000 * 100 });
+    await repo.save({ name: 'padaria joia de cocaia', limit: 100000 * 100 });
+    await repo.save({ name: 'kid mais', limit: 5000 * 100 });
   }
 }
